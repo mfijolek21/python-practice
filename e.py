b15b2bb993b863057a41fbd3d2ea74463532b3b7
Maks Fijołek <56736548+mfijolek21@users.noreply.github.com>
2024-05-23 16:57:17 +0200

    Add files via upload

---
+++ e.py
@@ -0,0 +1,6 @@
+a = input()
+if(a % 2 == 0):
+    print(f'{a} is an even number!')
+elif(a % 2 !=  0):
+    print(f'"A" is an odd number')
+
---
+++ tut5.py
@@ -0,0 +1,7 @@
+a = input()
+a = int(a)
+
+if(a%2 ==0):
+    print('Liczba jest parzysta')
+else:
+    print('Liczba jest nieparzysta')
